021f8a9a7f375117fc867efa58e0fad1c2fc09cc
James <james@quadrophone.com>
2017-02-26 15:42:17 -0800

    update all the things

--- js/auto-contract.js
+++ js/auto-contract.js
@@ -1,78 +1,20 @@
-$("html").attr("ng-app", "");
-$(document).ready(function() {
-  $('input').click(function(){
-    if(document.getElementById('individual').checked) {
-     $('div.contract span').empty();
-     $('span.wewill').append("I'll");
-     $('span.we').append("I");
-     $('span.us').append("me");
-     $('span.our').append("my");
-     $('span.were').append("I'm");
-   }
-   if(document.getElementById('company').checked) {
-    $('div.contract span').empty();
-    $('span.wewill').append("we'll");
-    $('span.were').append("we're");
-    $('span.we').append("we");
-    $('span.us').append("us");
-    $('span.our').append("our");    
-  }
-  if(document.getElementById('hourly').checked) {
-    $('span.worktype').empty().append('hourly rate')
-  }
-  if(document.getElementById('fixed').checked) {
-    $('span.worktype').empty().append('fixed price')
-  }
-});
 
-  $('#generate').click(function(){
-    $("span, p").each(function() {
-      var text = $(this).text();
-      text = text.replace(". we", ". We");
-      text = text.replace(". we'll", ". We'll");
-      text = text.replace(". we're", ". We're");
-      text = text.replace(". us", ". Us");
-      text = text.replace(". our", ". Our");
-      text = text.replace(". my", ". My");
-      $(this).text(text);
-      $('div.form').empty();
-      $('div.contract').show();
-      window.scrollTo(0,0);
-    });
-  });  
+var app = angular.module('contractKiller', ['ngSanitize']);
 
-
-  (function() {
-    $('input').keyup(function() {
-      var empty = false;
-      $('input').each(function() {
-        if ($(this).val() == '') {
-          empty = true;
-        }
-      });
-      if (empty) {
-        $('#generate').attr('disabled', 'disabled'); 
-      } else {
-        $('#generate').removeAttr('disabled'); 
-      }
-    });
-  })()
-
-
-  $('#disabled').click(function(){
-    if ($('#generate').attr('disabled')) {
-      alert("Please fill out all fields");
-    }
-  });
-  $('#print').click(function(){
+app.controller('ContractController', function($scope, $sanitize) {
+  $scope.contractorName = '';
+  $scope.contractorAddress = '';
+  $scope.clientName = '';
+  $scope.clientAddress = '';
+  $scope.price = '';
+  $scope.jurisdiction = '';
+  $scope.paymentSchedule = '';
+  $scope.paymentMethod = '';
+  $scope.overDuePenalty = '';
+  $scope.rateType = 'hourly';
+  $scope.date = '';
+  
+  $scope.print = function() {
     window.print();
-  })
+  }
 });
-
-
-
-
-
-
-
-
